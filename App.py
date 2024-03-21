10c1360f8dba34481a8f4e1248478589566db4b4
Steven Ngo <steven.ngo.077@gmail.com>
2024-03-21 12:59:51 -0700

    Initial commit

---
+++ App.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from rembg import remove
+from PIL import Image
+from io import BytesIO
+
+# App description
+st.markdown('''
+# Image Background Remover
+
+- Source Code: https://github.com/steven-ngo/Image-Background-Remover
+- Language: `Python`
+- Libraries: `streamlit` `rembg` `Pillow`
+''')
+st.write('---')
+
+uploaded = st.file_uploader('Upload an image Here', type=[ 'jpg', 'png', 'jpeg'])
+
+st.markdown('\n')
+
+col1, col2 = st.columns(2)
+
+if (uploaded):
+   if uploaded.size > 5242880:  #if greater than 5MB
+        st.error("The Image is too large. Must less than 5MB.")
+   else:
+      original = Image.open(uploaded)
+      edited = remove(original)
+
+      buf = BytesIO()
+      edited.save(buf, format="PNG")
+      
+      st.download_button("Download edited image", buf.getvalue(), "edited.png", "image/png")
+
+
+      col1.image(original, caption='Original')
+      col2.image(edited,caption='Edited')
+
+else:
+   col1.image('./cat.png', caption='Original')
+   col2.image('./edited.png',caption='Edited')
---
+++ README.md
@@ -0,0 +1,33 @@
+## US-National-Debt
+
+##  Demo
+
+https://stevengo-background-remover.streamlit.app/
+
+## Dependencies
+streamlit==1.32.0 \n
+rembg==2.0.50   \n
+Pillow==9.3.0  \n
+
+## Run the App Locally
+### Create conda environment
+```
+conda create -n app python=3.10.0
+```
+```
+conda activate app
+```
+### Install Dependencies
+
+Download requirements.txt file
+
+Pip install libraries
+```
+pip install -r requirements.txt
+```
+
+###  Launch the app
+
+```
+streamlit run App.py
+```
